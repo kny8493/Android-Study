c44a4a04e89a60a17583bac9944c71716c41b143
nayoung <kny8493@naver.com>
2020-07-22 23:35:22 +0900

    할일 앱 개발 - CRUD 구현 - ListView구현 - sharedPerence를 이용한 데이터 저장

---
+++ ToDoListApplication/app/src/main/java/com/example/todolistapplication/ListViewItem.java
@@ -0,0 +1,15 @@
+package com.example.todolistapplication;
+
+import com.kyleduo.switchbutton.SwitchButton;
+
+public class ListViewItem {
+    private SwitchButton switchButton;
+
+    public SwitchButton getSwitchButton() {
+        return switchButton;
+    }
+
+    public void setSwitchButton(SwitchButton switchButton) {
+        this.switchButton = switchButton;
+    }
+}
